fbfedf30ba639c705479f467bd943b6014e8acc8
cclamb <cclamb@cs.unm.edu>
2011-07-17 20:55:02 -0600

    work

--- project/lib/exServer.js
+++ project/lib/exServer.js
@@ -1,11 +1,15 @@
 var net = require('net');
 
 net.createServer(function(socket) {
+
 	socket.write('test server: 1.0.0; enter command\r\n', 'ascii', function() { 
 		console.log('...welcome sent to ' + socket.remotePort + '@' + socket.remoteAddress); 
 	});
+	
 	socket.on('data', function(data) {
 		console.log('data received: "' + data + '"');
 	});
+	
 	socket.pipe(socket);
+	
 }).listen(1337, "127.0.0.1", function() { console.log('...server has been bound.'); });
