519f76609cfffceea75685575c59fd5e6db758f8
Chris Lamb <cclamb@cs.unm.edu>
2011-08-28 12:36:17 -0600

    added jasmine-node bdd tests

---
+++ project/spec/randomSpec.coffee
@@ -0,0 +1,5 @@
+
+describe 'jasmine-node-coffee', ->
+
+  it 'should pass', ->
+    expect(1+2).toEqual(3)
---
+++ project/spec/random_spec.js
@@ -0,0 +1,29 @@
+
+var random = require('../lib/random').random;
+
+var SEED = 10;
+var LOW_SEED = 2;
+var MED_SEED = 50;
+var HIGH_SEED = 1000;
+
+var evaluator = function(seed) {
+	var generator = new random.Generator(seed);
+	var value = generator.generate();
+	expect(value).not.toBeNull();
+	expect(value).toBeGreaterThan(0);
+	expect(value).toBeLessThan(seed);
+};
+
+describe('random specification', function() {
+	
+	it('should return a value between 0 and the seed', function() {
+		evaluator(SEED);
+	});
+	
+	it('should work with a variety of seed values', function() {
+		evaluator(LOW_SEED);
+		evaluator(MED_SEED);
+		evaluator(HIGH_SEED);
+	});
+	
+});
